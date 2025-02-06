15f5cc3a6769adcac6fb2415ed34ba853a355a86
MariaEduardaKnupp <mariaeduardaknuppratto@gmail.com>
2025-02-06 17:17:53 -0300

    Atualizações da revisão

--- revizona/src/components/Demo1.jsx
+++ revizona/src/components/Demo1.jsx
@@ -1,9 +1,40 @@
+import { useState } from 'react';
 import './Demo.css'
 
 function Demo1() {
+    const [estado, setEstado] = useState(0)
+
+    function incrementaestado(){
+        setEstado(estado + 1)
+    }
+
+    function decrementa(){
+        setEstado(estado - 1)
+    }
+
+    let cornometro = 9;
+    function incrementacornometro(){
+        cornometro++
+        console.log(cornometro);
+    }
+    function alertarUsuario(){
+      alert("Corra para as montanhas")
+    }
   return (
     <div className='container-demo1'>
-      Demo1
+      <h2>Demo1</h2>
+      <button onClick={alertarUsuario}>Clica em mim</button>
+      <button onClick={() => {alert("Executei uma arrow function")}}>Ex Arrow Function</button>
+
+    <div>
+        Cornometro: {cornometro}
+        <button onClick={incrementacornometro}>+</button>
+    </div>
+    <div>
+        estado: {estado}
+        <button onClick={incrementaestado}>+</button>
+        <button onClick={decrementa}>-</button>
+    </div>
     </div>
   )
 }

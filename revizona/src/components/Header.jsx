65d658a556f4a40cdacc4a18fa0af52b1be3dfeb
MariaEduardaKnupp <mariaeduardaknuppratto@gmail.com>
2025-02-06 16:39:00 -0300

    Revisão da segunda fase

---
+++ revizona/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import './header.css'
+
+function Header() {
+  return (
+    <div className="container-header">
+      <p>LoGo</p>
+      <p>Link1</p>
+      <p>Link2</p>
+      <p>Link3</p>
+    </div>
+  )
+}
+
+export default Header
